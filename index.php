d6dc285cb2bb39a4d2db09a33343c2cd46fe28b6
Cedric Batina <shakagarvey@gmail.com>
2024-04-25 21:22:32 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$date = date("d/m/yy");
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+ <!-- Include the Font Awesome CSS file -->
+
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+ <link href="./stylefile.css" rel="stylesheet">
+ <title>Document</title>
+</head>
+
+<body>
+ <div>
+  h3.
+ </div>
+
+</body>
+
+</html>
